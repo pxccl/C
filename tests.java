3e6a3cb6a0aecf8dbe39744db1ac1de7cc51ebc8
pxc <pxccl@sina.com>
2015-05-22 10:51:51 +0800

    bug fix

--- tests.java
+++ tests.java
@@ -3,4 +3,5 @@ public class Tests
 	private static final String SHOW = "";
 	private static final String MASTER = "master";
 	private static final String DEV = "dev";
+	private static final String BUG = "bug";
 }
